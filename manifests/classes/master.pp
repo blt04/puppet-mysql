8850bfd10592bc8f0f94f6f68dc96d39255c8d0d
Brandon Turner <bt@brandonturner.net>
2012-08-20 09:55:38 -0500

    Allow configuring binary log file name

--- manifests/classes/master.pp
+++ manifests/classes/master.pp
@@ -1,8 +1,11 @@
 class mysql::master inherits mysql::server {
 
+  if $mysql_serverid {} else { $mysql_serverid = 1 }
+  if $mysql_binlog {} else { $mysql_binlog = "mysqld-bin" }
+
   Augeas["my.cnf/replication"] {
     changes => [
-      "set log-bin mysqld-bin",
+      "set log-bin ${mysql_binlog}",
       "set server-id ${mysql_serverid}",
       "set expire_logs_days 7",
       "set max_binlog_size 100M"
